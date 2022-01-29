9eca6152ed4832c2919bede85c7e33ffa8e5a4ac
Mario Haleem <mariohaleem@gmail.com>
2022-01-29 16:33:51 +0200

    add some commands

--- reactnd-contacts-server/bin/deploy.sh
+++ reactnd-contacts-server/bin/deploy.sh
@@ -1,2 +1,4 @@
-sudo eb init contacts-app --platform node.js --region us-east-1
-sudo eb create
+eb init $EB_ENV --platform node.js --region us-east-1
+eb use $EB_APP
+eb deploy $EB_ENV
+# eb create
